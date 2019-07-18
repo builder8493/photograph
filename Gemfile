2297021ba97ef55e2c468cd9e3ee0a90e028afa5
Your Name <kanda413@icloud.com>
2019-07-18 10:52:17 +0900

    Localize Japanese

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'jquery-rails'
 gem 'rb-readline'
 gem 'kaminari'
 gem 'kaminari-bootstrap'
+gem 'rails-i18n'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Photograph
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
+    config.i18n.default_locale = :ja
     
     config.action_view.field_error_proc = proc { |html_tag, instance| html_tag }
     # Settings in config/environments/* take precedence over those specified here.
